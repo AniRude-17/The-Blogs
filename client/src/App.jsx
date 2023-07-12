0b49dd6df30408f450eaf1c30bb31adf6876bfeb
anir-ab <anibhagwat17@gmail.com>
2023-07-13 01:58:12 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,58 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import SingleBlog from "./pages/SingleBlog";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+      {
+        path:"/post/:id",
+        element:<SingleBlog/>
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router}/>
 }
 
 export default App;
