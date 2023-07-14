a082cd06f509962445946360ab33e44f6970d2e3
anir-ab <anibhagwat17@gmail.com>
2023-07-14 19:52:17 +0530

    single post design

--- client/src/pages/SingleBlog.jsx
+++ client/src/pages/SingleBlog.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const SingleBlog = () => {
   return (
-    <div className='singleBlog'>SingleBlog xssssssdcfvgbhnjmkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</div>
+    <div className='single'>
+      <div className='content'>
+        <img src="https://picsum.photos/900/900"/>
+        <div className="user">
+          <img src="https://picsum.photos/100/100"/>
+          <div className="info">
+            <span>By Rude</span>
+            <p>Posted 449 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to="/write?edit=2">
+              Edit
+            </Link>
+            <Link to="/write?edit=2">
+              Delete
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
