58a194d2c9b775e7bf009f9c5489fa48586239e1
anir-ab <anibhagwat17@gmail.com>
2023-07-14 20:32:44 +0530

    write page and react-quill

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import {React, useState} from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'
 
 const Write = () => {
+  const [value, setValue] = useState('');
+  console.log(value);
   return (
     <div className='write'>
       <div className="content">
-        content
+      <input type='text' placeholder='title'/>
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
       </div>
       <div className="menu">
-        <div className="item">item</div>
+        <div className="item">
+          <h1>Publish</h1>
+          <span><b>Status :</b>Draft </span> 
+          <span><b>Visibility :</b>Public </span> 
+          <input style={{display:"none"}} type='file' id='file' name=""/>
+          <label htmlFor='file'>Upload Image</label>
+          <div className='buttons'>
+            <button>Save as Draft</button>
+            <button> Update</button>
+          </div>
+        </div>
+
         <div className="item">item</div>
       </div>
     </div>
