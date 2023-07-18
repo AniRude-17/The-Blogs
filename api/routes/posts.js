0bc700e2c35248695f3a060e42fd5377c047d29a
anir-ab <anibhagwat17@gmail.com>
2023-07-19 00:03:07 +0530

    homepage

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,16 @@
 import express from "express";
 
-const router=express.Router()
+import { addPost,deletePost,getPost,getPosts,updatePost } from "../controllers/post.js";
 
-export default router
+
+// Create a new Router object
+const router = express.Router();
+
+// Define routes for various HTTP methods and their corresponding functions
+router.get("/", getPosts); // Get all posts
+router.get("/:id", getPost); // Get a specific post by its ID
+router.post("/", addPost); // Add a new post
+router.delete("/:id", deletePost); // Delete a post by its ID
+router.put("/:id", updatePost); // Update a post by its ID
+
+export default router;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,54 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
-  
-  const posts=[
-    {
-      id:1,
-      title:"lONE BLOG A DAY ABCD EFGHIJK LMN OP QRSTUVWXY XZAB",
-      desc:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      img:"https://picsum.photos/900/900",
-    },
-    {
-      id:2,
-      title:"RFTVGBHNJMK",
-      desc:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      img:"https://picsum.photos/900/900",
-    },
-    {
-      id:3,
-      title:"RANDOM TITLE 222",
-      desc:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      img:"https://picsum.photos/900/900",
-    },
-    {
-      id:4,
-      title:"tITLE RANDOME",
-      desc:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      img:"https://picsum.photos/900/900",
-    },
-  ];
+  // Declaring a state variable called posts and initializing it to an empty array
+  const [posts, setPosts] = useState([]);
 
+  // Getting the current URL query string (if any) using the useLocation hook from react-router-dom
+  const cat = useLocation().search;
 
+  // Defining an effect that runs when the cat variable changes
+  useEffect(() => {
+    // Defining an asynchronous function called fetchData
+    const fetchData = async () => {
+      try {
+        // Making an HTTP GET request to the server to retrieve posts data based on the cat variable
+        const res = await axios.get(`http://localhost:1234/api/posts${cat}`);
+        // Updating the posts state variable with the retrieved data
+        setPosts(res.data);
+      } catch (err) {
+        // Logging any errors that occur during the request
+        console.log(err);
+      }
+    };
+    // Calling the fetchData function
+    fetchData();
+  }, [cat]); // Specifying that this effect should only run when the cat variable changes
+
+  // Defining a helper function called getText that takes an HTML string and returns the text content
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+
+  // Rendering the Home component
   return (
-    <div className='home'>
+    <div className="home">
       <div className="posts">
-        {posts.map(post=>(
-          <div className='post' key={post.id}>
-            <div className="img">
-              <img src={post.img} alt="haha"/>
+        {/* Mapping over the posts state variable and rendering a Post component for each post */}
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="post-img">
+              {/* Rendering the post image */}
+              <img src={`${post.img}`} alt="post cover" />
             </div>
-            <div className='content'>
-              <Link to="/post/${post.id}" className="title">{post.title}</Link>
-              <p>{post.desc}</p>
-              <button>Read More</button>
-          </div>
+            <div className="content">
+              {/* Rendering a link to the post page */}
+              <Link className="link" to={`http://localhost:1234/api/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              {/* Rendering the post description */}
+              <p>{getText(post.desc)}</p>
+              {/* Rendering a button to read more */}
+              <Link className="link" to={`http://localhost:1234/api/post/${post.id}`}>
+                <button>Read More</button>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
