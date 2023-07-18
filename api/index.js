c2f2303f2c90c77d5ad42f21b2f8d3f9e95e3177
anir-ab <anibhagwat17@gmail.com>
2023-07-18 18:09:50 +0530

    working login/register

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 const app = express()
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/posts",postRoutes)
 app.use("/api/auth",authRoutes)
