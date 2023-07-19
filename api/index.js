e7a32f787f5cadccd97f50cb87ac731300b6317f
anir-ab <anibhagwat17@gmail.com>
2023-07-19 17:05:42 +0530

    write page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 
 const app = express()
@@ -15,7 +16,32 @@ app.use("/api/posts",postRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Set the destination folder where the uploaded file should be stored
+      cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+      // Set the filename of the uploaded file
+      cb(null, Date.now() + file.originalname);
+    },
+  });
+  
+  // Set up multer middleware with the defined storage configuration
+  const upload = multer({ storage });
+  
+  // Set up a POST endpoint for handling file uploads
+  app.post("/api/upload", upload.single("file"), function (req, res) {
+    // Get the uploaded file
+    const file = req.file;
+    // Send a response with the filename of the uploaded file
+    res.status(200).json(file.filename);
+  });
 
+
+  app.get('/test' ,(req,res)=> {
+    res.json("Yo this is test")
+  })
 app.listen(1234,()=>{
     console.log("haha")
 })
